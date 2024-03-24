0d3b96ae91dfaebe3b5109b2ad8a7162c02a4883
priyanshuprasad22 <priyanshuprasad925@gmail.com>
2024-03-24 16:25:55 +0530

    Second Commit

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express=require('express');
 const dotenv=require('dotenv');
 const connectDB= require('./config/db');
-const routes= require('./routes/events')
+const routes= require('./routes/events');
+
+const helmet= require('helmet');
+const cors=require('cors');
+const xss= require('xss-clean');
 
 const errorHandler = require('./middleware/errorhandler')
 const notfound = require('./middleware/not-found');
@@ -10,7 +14,11 @@ const notfound = require('./middleware/not-found');
 dotenv.config();
 const app=express();
 
+app.set('trust proxy',1);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 
 connectDB();
